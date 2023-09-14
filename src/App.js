687fd645beef5584ee308eb21133d1adf9258b0f
nour hellani <nour.hellani@creoshift.com>
2023-09-14 12:33:23 +0300

    fixes

--- src/App.js
+++ src/App.js
@@ -6,13 +6,13 @@ function App() {
   return (
     <div>
       <div className="bg-[#000080] text-white">
-        <div className="sm:mx-20 mx-10">
+        <div className="mx-10">
           <h1 className="py-5 font-bold text-xl">
             SEO Best Practice Guideline
           </h1>
         </div>
       </div>
-      <div className="sm:mx-20 mx-10 pb-10">
+      <div className=" mx-10 pb-10">
         <CheckboxList />
       </div>
     </div>
